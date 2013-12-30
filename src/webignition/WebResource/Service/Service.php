66f5c91b52a6741da716c5fedd7078f3228c6796
Jon Cram <jon@webignition.net>
2013-12-30 12:06:50 +0000

    Initial barebones

---
+++ src/webignition/WebResource/Service/Service.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace webignition\WebResource\Service;
+
+class Service {
+    
+}
---
+++ tests/webignition/Tests/WebResource/Service/ServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace webignition\Tests\WebResource;
+
+use webignition\WebResource\Service\Service as WebResourceService;
+
+class ServiceTest extends BaseTest {
+    
+    
+    public function testTest() {
+        $webResourceService = new WebResourceService();
+    }
+    
+}
